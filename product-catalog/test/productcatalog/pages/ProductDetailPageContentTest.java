28151435cbdbec22481e59e4bcac60a292fcc1ca
Peter Gerhard <peter.gerhard@commercetools.de>
2015-08-25 16:18:27 +0200

    Add pdpContentTest

--- product-catalog/test/productcatalog/pages/ProductDetailPageContentTest.java
+++ product-catalog/test/productcatalog/pages/ProductDetailPageContentTest.java
@@ -91,7 +91,7 @@ public class ProductDetailPageContentTest {
     }
 
     @Test
-    public void deliveryJson() throws IOException {
+    public void shippingRateJson() throws IOException {
         final ShopShippingRate dhl = new ShopShippingRate("DHL", ShippingRate.of(Money.of(10, "EUR")));
         final ShopShippingRate dhlFreeAbove = new ShopShippingRate("DHL", ShippingRate.of(Money.of(10, "EUR"), Money.of(50, "EUR")));
         final ShippingRateDataFactory shippingRateDataFactory = ShippingRateDataFactory.of(priceFormatter);
@@ -115,6 +115,41 @@ public class ProductDetailPageContentTest {
 
         final JsonNode expected = readJsonNodeFromResource("suggestionData.json").get("suggestions");
         final JsonNode result = toJsonNode(suggestionData);
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void pdpJson() throws IOException {
+        final CmsPage cms = (messageKey, args) -> Optional.of(messageKey);
+        final PdpStaticData pdpStaticData = new PdpStaticData(cms, BagItemDataFactory.of().create(100), RatingDataFactory.of(cms).create());
+
+        final Category woman = categories.findById("33339d11-0e7b-406b-899b-60f4c34c2948").get();
+        final Category bags = categories.findById("32952779-d916-4f2b-b1d5-9efd7f7b9f58").get();
+        final Category handBags = categories.findById("9a584ee8-a45a-44e8-b9ec-e11439084687").get();
+        final List<Category> breadcrumbs = asList(woman, bags, handBags);
+        final CategoryLinkDataFactory categoryLinkDataFactory = CategoryLinkDataFactory.of(translator);
+        final List<LinkData> breadcrumbData = breadcrumbs.stream().map(categoryLinkDataFactory::create).collect(toList());
+
+        final ProductProjection product = readObjectFromResource("product.json", ProductProjection.typeReference());
+        final ProductVariant variant = product.getMasterVariant();
+        final ProductData productData = ProductDataFactory.of(userContext).create(product, variant);
+
+        final ShopShippingRate dhl = new ShopShippingRate("DHL", ShippingRate.of(Money.of(10, "EUR")));
+        final ShopShippingRate dhlFreeAbove = new ShopShippingRate("DHL", ShippingRate.of(Money.of(10, "EUR"), Money.of(50, "EUR")));
+        final ShippingRateDataFactory shippingRateDataFactory = ShippingRateDataFactory.of(priceFormatter);
+        final List<ShippingRateData> shippingRateData = asList(dhl, dhlFreeAbove).stream().map(shippingRateDataFactory::create).collect(toList());
+
+        final ProductProjection selma = getProductById(products, "4f643a44-5bed-415e-ae60-64c46bfb26f5");
+        final ProductProjection dkny = getProductById(products, "a3f4588e-fcfe-41de-bd09-a071d76d697d");
+        final ProductProjection miabag = getProductById(products, "dc9a4460-491c-48b4-bcf6-1d802bb7e164");
+        final ProductProjection altea = getProductById(products, "4f643a44-5bed-415e-ae60-64c46bfb26f5");
+        final ProductDataFactory productDataFactory = ProductDataFactory.of(userContext);
+        final List<ProductData> suggestionData = asList(selma, dkny, miabag, altea).stream()
+                .map(p -> productDataFactory.create(p, p.getMasterVariant())).collect(toList());
+
+        final JsonNode expected = readJsonNodeFromResource("pdpContent.json").get("content");
+        final JsonNode result = toJsonNode(new ProductDetailPageContent("", pdpStaticData, breadcrumbData, productData, shippingRateData, suggestionData));
 
         assertThat(result).isEqualTo(expected);
     }
